86911c9574554fb1bed59ff458303d7f44134175
evgesha <zplacesound@gmail.com>
2020-06-02 14:03:47 +0300

    in smart-grid path

--- smartgrid.js
+++ smartgrid.js
@@ -1,8 +1,8 @@
 module.exports = {
                 columns: 12,
                   offset: "30px",
-                breakPoints: {},
-              container:{
-                maxWidth: "1140px",
-                fields: "150px"}
-              }
+
+                    container:{
+                      maxWidth: "1140px",
+                      fields: "165px"}
+                    }
