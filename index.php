f44dc764c41f388d05bedaecd3f068feda89c2c3
evgesha <zplacesound@gmail.com>
2020-06-04 17:24:48 +0300

    in some process

--- index.php
+++ index.php
@@ -8,25 +8,27 @@
      </div>
    </div>
  </div>
- <div class="we-are-fishfish">
-    <div class="left-place">
-      <p class="left-text">
-        Кто мы такие?
-      </p>
-    </div>
-    <div class="right-place">
-      <div class="about-text">
-        <p class="some-paragraph">Привет, мы команда IT-специалистов <br><span class="important-place">FISHFISH</span></p>
-        <br>
-        <p class="some-paragraph">Мы ценим простоту, понятность и удобство. В работе мы прежде всего руководствуемся этими принципами.</p>
-        <br>
-        <p class="some-paragraph">Если тебе важно быть в курсе всего процесса, если ты знаешь чего хочешь и если ты понимаешь, что лучший продукт создается при тесном взаимодействии заказчика и исполнителя, то мы рады будем с тобой поработать!</p>
+ <div class="who-is">
+   <div class="we-are-fishfish">
+      <div class="left-place">
+        <p class="left-text">
+          Кто мы такие?
+        </p>
       </div>
-    </div>
+      <div class="right-place">
+        <div class="about-text">
+          <p class="some-paragraph">Привет, мы команда IT-специалистов <br><span class="important-place">FISHFISH</span></p>
+          <br>
+          <p class="some-paragraph">Мы ценим простоту, понятность и удобство. В работе мы прежде всего руководствуемся этими принципами.</p>
+          <br>
+          <p class="some-paragraph">Если тебе важно быть в курсе всего процесса, если ты знаешь чего хочешь и если ты понимаешь, что лучший продукт создается при тесном взаимодействии заказчика и исполнителя, то мы рады будем с тобой поработать!</p>
+        </div>
+      </div>
+   </div>
  </div>
  <div class="what-we-do">
    <div class="left-place">
-    <p class=left-text>Что мы делаем?</p>
+    <p class="left-text">Что мы делаем?</p>
    </div>
    <div class="right-place">
       <div class="links">
@@ -34,20 +36,63 @@
           <a href="">Дизайн</a>
           <a href="">Разработка</a>
       </div>
-
    </div>
  </div>
-    <!---
+ <!---
+ <div class="cases">
+   <div class="left-place">
+       <div class="left-place">
+          <p class="left-text">Кейсы </p>
+       </div>
+   </div>
+   <div class="right-place">
+      <div class="cases-place">
+          <div class="case-name">
+            Canyon Development
+          </div>
+          <div class="case-discription">
+              Компания по производству электроники
+          </div>
+          <div class="khow-more">
+            <a href="#">more</a>
+          </div>
+      </div>
+   </div>
+ </div>
+ <div class="prices">
+   <div class="left-place">
+      <p class="left-text">Цены </p>
+   </div>
+   <div class="right-place">
+       <div class="links">
+           <a href="">Сайт под ключ</a>
+           <a href="">Дизайн</a>
+           <a href="">Разработка</a>
+       </div>
+   </div>
+ </div>
+<div class="proposal-form">
   <aside>
+    <p>Оставь нам заявку</p>
       <form action="">
           <input type="hidden">
+
+          <div class="name-chain">
+              <p>Mеня зовут </p>
+              <input type="text" name="name">
+          </div>
+          <div class="email-chain">
+              <p>Вы можете связаться со мной по почте </p>
+              <input type="text" name="email">
+          </div>
           <div class="phone-chain">
-              <p>Phone</p>
+              <p>и по номеру</p>
               <input type="text" name="phone">
           </div>
           <input type="button" class="ff-btn-app" value="Отправить">
         </form>
   </aside>
-  -->
+</div>
 
+--->
  <?php get_footer() ?>
